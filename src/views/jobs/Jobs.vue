485fd9523b296c82a72afa10911fd295592abb4d
George Octavius <113178814+georgetheoracle@users.noreply.github.com>
2026-02-25 13:06:24 -0500

    update

--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -1,7 +1,10 @@
 <template>
   <h1>Jobs</h1>
-  <div v-for="job in jobs" :key="job.id">
+  <div v-for="job in jobs" :key="job.id" class="job">
+    <router-link :to="{name: 'JobDetails', params: {id: job.id}}">
+
     <h2>{{ job.title }}</h2>
+    </router-link>
   </div>
 </template>
 
@@ -20,5 +23,19 @@ data(){
 </script>
 
 <style>
-
+    .job h2{
+        color: #444;
+        background: #f4f4f4;
+        padding: 20px;
+        border-radius: 10px;
+        margin: 10px auto;
+        max-width: 600px;
+        cursor: pointer; 
+    }
+    .job h2:hover{
+        background: #ddd;
+    }
+    .job a{
+        text-decoration: none;
+    }
 </style>
