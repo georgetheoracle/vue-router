e08eb40b3d85d5bb3c5fc61a5ddfbf0e7624cb29
George Octavius <113178814+georgetheoracle@users.noreply.github.com>
2026-02-25 12:13:54 -0500

    update

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea enim obcaecati dolore optio soluta provident totam ipsam praesentium? Facilis, cumque?</p>
+
   </div>
 </template>
